368508ed350b83a5e8f4c084870699bf5e949f35
BestVersion7 <hunterkfan@gmail.com>
2024-01-06 17:59:01 -0500

    rewrote all api routes

--- app/utils/apiCalls.ts
+++ app/utils/apiCalls.ts
@@ -1,5 +1,4 @@
-const stripeOrigin = "https://www.hunterkf.com/api/v2/stripe";
-const cartOriginServer = "https://www.hunterkf.com/api/v2/cart";
+import { CartType, CustomerType } from "../types";
 
 const options = {
     headers: {
@@ -7,11 +6,14 @@ const options = {
     },
 };
 
+const stripeOrigin = "/api/stripe";
+const cartOrigin = "/api/cart";
+const cartCookieOrigin = "/api/cartcookie";
+
 export const getAllProducts = async () => {
     const res = await fetch(`${stripeOrigin}/product`, {
         ...options,
-        // next: { revalidate: 60 * 60 * 24 * 1 },
-        cache: "no-store",
+        next: { revalidate: 60 * 60 * 24 * 1 },
     });
     const { data } = await res.json();
     return data;
@@ -41,8 +43,8 @@ export const getInvoiceById = async (id: string) => {
     return data;
 };
 
-export const getCartTotal = async (cookie: string | undefined) => {
-    const res = await fetch(`${cartOriginServer}/total?cookie_id=${cookie}`, {
+export const getCartTotal = async () => {
+    const res = await fetch(`${cartOrigin}/total`, {
         ...options,
         cache: "no-cache",
     });
@@ -50,10 +52,83 @@ export const getCartTotal = async (cookie: string | undefined) => {
     return data;
 };
 
-export const getCartByCookie = async (cookie: string | undefined) => {
-    const res = await fetch(`${cartOriginServer}?cookie_id=${cookie}`, {
+export const getCartByCookie = async () => {
+    const res = await fetch(cartOrigin, {
         ...options,
         cache: "no-cache",
+    });
+    const data = await res.json();
+    return data;
+};
+
+export const createCartCookie = async () => {
+    const res = await fetch(cartCookieOrigin, { ...options, method: "post" });
+    const data = await res.json();
+    return data;
+};
+
+export const deleteCartCookie = async () => {
+    const res = await fetch(cartCookieOrigin, { ...options, method: "delete" });
+    const data = await res.json();
+    return data;
+};
+
+export const createCart = async (cart: CartType) => {
+    const res = await fetch(cartOrigin, {
+        ...options,
+        method: "POST",
+        body: JSON.stringify(cart),
+    });
+    const data = await res.json();
+    return data;
+};
+
+export const updateCart = async (
+    cart: Pick<CartType, "price_id" | "quantity">
+) => {
+    const res = await fetch(cartOrigin, {
+        ...options,
+        method: "PUT",
+        body: JSON.stringify(cart),
+    });
+    const data = await res.json();
+    return data;
+};
+
+export const deleteCartItemByPriceId = async (priceId: string) => {
+    const res = await fetch(cartOrigin, {
+        ...options,
+        method: "DELETE",
+        body: JSON.stringify({ price_id: priceId }),
+    });
+    const data = await res.json();
+    return data;
+};
+
+export const getPaymentIntent = async (id: string) => {
+    const res = await fetch(`${stripeOrigin}?id=${id}`, { ...options });
+    const data = await res.json();
+    return data;
+};
+
+export const createCustomer = async (customerData: CustomerType) => {
+    const res = await fetch(`${stripeOrigin}/customer`, {
+        ...options,
+        method: "post",
+        body: JSON.stringify(customerData),
+    });
+    const data = await res.json();
+    return data;
+};
+
+export const createInvoice = async (
+    customer: string,
+    invoiceData: CartType[]
+) => {
+    const res = await fetch(`${stripeOrigin}`, {
+        ...options,
+        method: "post",
+        body: JSON.stringify({ customerId: customer, invoiceData }),
     });
     const data = await res.json();
     return data;
--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        port: "",
-        pathname: "/crimson-flamingo/**",
-      },
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "res.cloudinary.com",
+                port: "",
+                pathname: "/crimson-flamingo/**",
+            },
+        ],
+    },
 };
 
 module.exports = nextConfig;
