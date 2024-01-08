b8dbbe4461028234339063221e8076d3d54df45b
BestVersion7 <hunterkfan@gmail.com>
2024-01-07 22:21:18 -0500

    update domain

--- app/utils/apiCalls.ts
+++ app/utils/apiCalls.ts
@@ -1,6 +1,6 @@
 import { CartType, CustomerType } from "../types";
 
-let host = "https://www.fashionstore-27tcv5bry-info-speedruntrav.vercel.app";
+let host = "https://afashionstore.vercel.app/";
 
 const stripeOrigin = `${host}/api/stripe`;
 const cartOrigin = `${host}/api/cart`;
