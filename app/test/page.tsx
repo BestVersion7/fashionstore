77e0fdf33a5f338cce5b7bb92ace5f6403750497
BestVersion7 <hunterkfan@gmail.com>
2024-01-22 15:30:55 -0500

    test

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-export default function TestPage(props: { quantity: number }) {
+export default function TestPage() {
     return (
         <main>
             <div className="relative"></div>
