5d862ded0d920998f1f43a713d77e4bc5d8abb6a
BestVersion7 <hunterkfan@gmail.com>
2024-01-27 01:04:49 -0500

    staticparan

--- app/shop/[category]/page.tsx
+++ app/shop/[category]/page.tsx
@@ -1,11 +1,9 @@
 import { getProductBySearchCategory } from "@/app/utils/apiCalls";
-import { ProductType } from "@/app/types";
-import { SearchInput } from "@/app/components/SearchInput";
 import { ProductFilter } from "@/app/components/ProductFilter";
 import { ProductMapped } from "@/app/components/ProductMapped";
 
 // All products not returned in the api call will be 404
-export const dynamicParams = false;
+// export const dynamicParams = false;
 
 export async function generateStaticParams() {
     return [
