50dc956b95ebb205c8bc01f3ce690a777915e737
BestVersion7 <hunterkfan@gmail.com>
2024-01-28 15:46:18 -0500

    add origin middleware for api routes

--- middleware.ts
+++ middleware.ts
@@ -9,17 +9,38 @@ import { getToken } from "next-auth/jwt";
 //     limiter: Ratelimit.slidingWindow(20, "1 s"),
 // });
 
+let availableOrigins = "http://localhost:3000";
+if (process.env.NODE_ENV === "production") {
+    availableOrigins = "http://afashionstore.vercel.app";
+}
+
 export async function middleware(req: NextRequest) {
     const apiKey = req.headers.get("authorization");
-    if (req.nextUrl.pathname.startsWith("/api/order") && req.method === "GET") {
-        if (apiKey !== process.env.API_KEY) {
-            return NextResponse.json("unauthorized", { status: 401 });
-        } else {
+
+    if (req.nextUrl.pathname.startsWith("/api")) {
+        if (req.nextUrl.origin !== availableOrigins) {
+            return NextResponse.json("Origin not allowed", { status: 405 });
+        } else if (
+            req.nextUrl.pathname.startsWith("/api/order") &&
+            req.method === "GET"
+        ) {
+            if (apiKey !== process.env.API_KEY) {
+                return NextResponse.json("unauthorized", { status: 401 });
+            } else {
+                return NextResponse.next();
+            }
+        }
+    }
+
+    if (req.nextUrl.pathname.startsWith("/account")) {
+        const session = await getToken({ req });
+        if (!session) {
+            return NextResponse.redirect(new URL("/signin", req.url));
+        } else if (session) {
             return NextResponse.next();
         }
     }
 
-    const session = await getToken({ req });
     // if (req.nextUrl.pathname.startsWith("/api")) {
     //     const ip = req.ip ?? "127.0.0.1";
     //     const res = await ratelimit.limit(ip);
@@ -34,11 +55,4 @@ export async function middleware(req: NextRequest) {
     //         return NextResponse.next();
     //     }
     // }
-    if (req.nextUrl.pathname.startsWith("/account")) {
-        if (!session) {
-            return NextResponse.redirect(new URL("/signin", req.url));
-        } else if (session) {
-            return NextResponse.next();
-        }
-    }
 }
