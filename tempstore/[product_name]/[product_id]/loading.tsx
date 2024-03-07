0e6d6bd41a09724d8f416b5b3cc42fa1f2bb5a50
BestVersion7 <hunterkfan@gmail.com>
2024-03-06 19:13:13 -0500

    isolate

--- tempstore/[product_name]/[product_id]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { ImSpinner2 } from "react-icons/im";
-
-export default function Loading() {
-    return (
-        <main className="min-h-96">
-            <ImSpinner2 className="text-3xl text-orange-500" />
-        </main>
-    );
-}
